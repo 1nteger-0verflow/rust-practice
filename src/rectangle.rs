9f6609eb361b7ec9df6983929a0a4f3315543c7a
1nteger-0verflow <rs.10verflow@gmail.com>
2026-01-15 03:48:21 +0000

    add: unit tests

--- src/rectangle.rs
+++ src/rectangle.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub struct Rectangle {
     pub width: u32,
     pub height: u32,
@@ -17,5 +18,38 @@ impl Rectangle {
             width: size,
             height: size,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
     }
 }
